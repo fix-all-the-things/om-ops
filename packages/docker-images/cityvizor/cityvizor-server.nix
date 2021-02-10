99fa7c37906f1a2f99523de628943b0e793caea9
Richard Marko <srk@48.io>
2021-02-10 18:28:06 +0100

    bump cityvizor-server image

--- packages/docker-images/cityvizor/cityvizor-server.nix
+++ packages/docker-images/cityvizor/cityvizor-server.nix
@@ -1,7 +1,7 @@
 {
   imageName = "cityvizor/cityvizor-server";
-  imageDigest = "sha256:977ff8d22192fcfcc1ab5896a3ce7ac4433a8fe60ef96057ddda316c1e20e1f2";
-  sha256 = "106ar5pkrildxzkfsib89i54s2vyvfbf3fq2n5gc8lh0nmmkg2lz";
+  imageDigest = "sha256:71984dab1e860538e0c0f82c3fa288abf210bfde5b0dfa64e553f890c2517487";
+  sha256 = "1y3c9bh41p1lggbzrr2kdi6w070xn18qy9s7zvr047b77c6kgl5q";
   finalImageName = "cityvizor/cityvizor-server";
   finalImageTag = "latest";
 }
