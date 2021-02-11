20de6779dac6573b2d898a8525328d28e8b4d544
Richard Marko <srk@48.io>
2021-02-11 10:50:22 +0100

    data/hosts: add proxy, tidy

--- data/hosts.nix
+++ data/hosts.nix
@@ -1,22 +1,27 @@
-{
+rec {
   mesta-libvirt = {
     addr.pub = {
       ipv4 = "37.205.14.17";
       ipv6 = "2a03:3b40:fe:3d::";
     };
   };
+
+  proxy = mesta-libvirt;
+
   mesta-services = {
     addr.pub = {
       ipv4 = "37.205.14.138";
       ipv6 = "2a03:3b40:fe:32::";
     };
   };
+
   mesta-services-2 = {
     addr.pub = {
       ipv4 = "37.205.12.242";
       ipv6 = "2a03:3b40:fe:1b::1";
     };
   };
+
   status = {
     addr.pub = {
       ipv4 = "83.167.228.98";
@@ -24,17 +29,20 @@
     };
     addr.int.v4 = "172.16.9.18";
   };
+
   pg = {
     addr.pub = {
       ipv4 = "37.205.8.221";
       ipv6 = "2a01:430:17:1::ffff:1380";
     };
   };
+
   cv-beta = {
     addr.pub = {
       ipv6 = "2a03:3b40:fe:a4::1";
     };
   };
+
   cv-prod = {
     addr.pub = {
       ipv4 = "37.205.14.126";
