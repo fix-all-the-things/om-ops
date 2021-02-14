af1f9be4e1fa8744fb49d916affe0dd80c6aa312
Richard Marko <srk@48.io>
2021-02-14 18:55:39 +0100

    modules/deploy: update home-manager pin

--- modules/deploy.nix
+++ modules/deploy.nix
@@ -1,8 +1,8 @@
 { config, lib, pkgs, ... }:
 let
   home-manager-src = builtins.fetchTarball {
-    url = "https://github.com/nix-community/home-manager/archive/63f299b3347aea183fc5088e4d6c4a193b334a41.tar.gz";
-    sha256 = "0iksjch94wfvyq0cgwv5wq52j0dc9cavm68wka3pahhdvjlxd3js";
+    url = "https://github.com/nix-community/home-manager/archive/209566c752c4428c7692c134731971193f06b37c.tar.gz";
+    sha256 = "1canlfkm09ssbgm3hq0kb9d86bdh84jhidxv75g98zq5wgadk7jm";
   };
 in
 {
