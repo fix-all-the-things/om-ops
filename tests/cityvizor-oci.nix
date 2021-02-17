b76e41680d87bdf1306a02498f701dce8930e17c
Richard Marko <srk@48.io>
2021-02-17 08:42:19 +0100

    tests/cityvizor-oci: check worker, redundantInstances, increase disk size

--- tests/cityvizor-oci.nix
+++ tests/cityvizor-oci.nix
@@ -39,18 +39,24 @@ let
       networking.firewall.allowedTCPPorts = [ 5432 ];
 
       virtualisation.memorySize = 2*1024;
-      virtualisation.diskSize = 4096;
+      virtualisation.diskSize = 5*1024;
       virtualisation.cores = 2;
     };
 
-    testScript = ''
+    testScript = { nodes, ...}: let
+      serverInstances = nodes.machine.config.services.cityvizor.server.redundantInstances;
+      serverPort = nodes.machine.config.services.cityvizor.server.port;
+    in
+    ''
       machine.start()
       machine.wait_for_unit("postgresql.service")
       machine.wait_for_unit("${backend}-cv-server.service")
       machine.wait_for_unit("${backend}-cv-client.service")
       machine.wait_for_unit("${backend}-cv-landing-page.service")
+      machine.wait_for_unit("${backend}-cv-worker.service")
       machine.wait_for_unit("nginx.service")
-      machine.sleep(10)
+      machine.wait_for_open_port("${builtins.toString serverPort}")
+
       with subtest("check landing page redirect"):
           http_code = machine.succeed(
               f"curl -s -w '%{{http_code}}' --head --fail -v localhost"
@@ -72,6 +78,14 @@ let
                   "curl -s localhost/api/public/profiles/praha12 | ${pkgs.jq}/bin/jq"
               )
           )
+
+      ${lib.optionalString (nodes.machine.config.services.cityvizor.server.redundantInstances != 0)
+      ''
+      machine.wait_for_open_port("${builtins.toString (serverPort + 1)}")
+      with subtest("check access to redundant server instance(s)"):
+          print(machine.succeed("curl 127.0.0.1:${builtins.toString (serverPort + 1)}"))
+      ''}
+      # keep to avoid blank line linting
     '';
   };
 in
