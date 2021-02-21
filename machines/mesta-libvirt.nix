1eb4a3d4b3a088e6c5e1ed07d36b4ec03bcd0f0c
Richard Marko <srk@48.io>
2021-02-21 13:47:54 +0100

    machines/mesta-libvirt: drop SSH forwards to VMs, we can jumphost instead

--- machines/mesta-libvirt.nix
+++ machines/mesta-libvirt.nix
@@ -31,7 +31,6 @@ in
         network = "fda7:1646:3af8:666e::/64";
         hostAddress = "fda7:1646:3af8:666e::1";
         forwardPorts = [
-          { destination = "[fda7:1646:3af8:666e:5054:ff:fe27:cbe]:22";  sourcePort = 22; }
           { destination = "[fda7:1646:3af8:666e:5054:ff:fe27:cbe]:80";  sourcePort = 80; }
           { destination = "[fda7:1646:3af8:666e:5054:ff:fe27:cbe]:443"; sourcePort = 443; }
 
@@ -50,7 +49,6 @@ in
 
   networking.nat = {
      forwardPorts = [
-       { destination = "192.168.122.100:22";    sourcePort = 10022;} # mail ssh
        { destination = "192.168.122.100:25";    sourcePort = 25;}    # mail SMTP
        { destination = "192.168.122.100:110";   sourcePort = 110;}   # mail POP3
        { destination = "192.168.122.100:143";   sourcePort = 143;}   # mail IMAP
@@ -64,21 +62,15 @@ in
        { destination = "192.168.122.100:9154";  sourcePort = 10094;} # mail prometheus postfix collector
        { destination = "192.168.122.100:7980";  sourcePort = 10098;} # mail prometheus rspamd collector
 
-       { destination = "192.168.122.101:22"; sourcePort = 10122;}    # sympa ssh
        { destination = "192.168.122.101:9100";  sourcePort = 10191;} # sympa prometheus node collector
 
-       { destination = "192.168.122.102:22"; sourcePort = 10222;}    # midpoint ssh
-
-       { destination = "192.168.122.103:22"; sourcePort = 10322;}    # matomo ssh
        { destination = "192.168.122.103:9100";  sourcePort = 10391;} # matomo prometheus node collector
 
-       { destination = "192.168.122.104:22";  sourcePort = 10422;}   # proxy ssh
        { destination = "192.168.122.104:80";  sourcePort = 80;}      # proxy web
        { destination = "192.168.122.104:443"; sourcePort = 443;}     # proxy ssl
        { destination = "192.168.122.104:9100";  sourcePort = 10491;} # proxy prometheus node collector
        { destination = "192.168.122.104:9113";  sourcePort = 10493;} # proxy prometheus nginx collector
 
-       { destination = "192.168.122.105:22";  sourcePort = 10522;}   # mediawiki ssh
        { destination = "192.168.122.105:9100";  sourcePort = 10591;} # mediawiki prometheus node collector
      ];
   };
