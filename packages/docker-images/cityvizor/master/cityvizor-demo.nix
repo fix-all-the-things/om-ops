c83821d8bf414ea9ceffff0e4cbccc98029627ec
Richard Marko <srk@48.io>
2021-03-10 07:47:45 +0100

    dockerImages/cityvizor: add cityvizor-demo master

---
+++ packages/docker-images/cityvizor/master/cityvizor-demo.nix
@@ -0,0 +1,7 @@
+{
+  imageName = "cityvizor/cityvizor-demo";
+  imageDigest = "sha256:b519c5f266d7f49fcc40dc48dd62a8f0e897151b5dad5481f1d5a8ff1e545d1e";
+  sha256 = "04y1bjawa4m63nrdj7izqj43zr0kyaqrppa7jrj7jnwil31vwq9f";
+  finalImageName = "cityvizor/cityvizor-demo";
+  finalImageTag = "master";
+}
