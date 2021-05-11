5b888999c51ce9b5448e7f18662721e914c65dac
Richard Marko <srk@48.io>
2021-05-11 16:15:57 +0200

    wireguard: add preferIPv4 option

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -16,6 +16,7 @@ in
           "${data.hosts.${config.networking.hostName}.addr.priv.ipv6}/128"
         ];
       };
+
       allowedIPs = mkOption {
         type = types.listOf types.str;
         default = [ "10.23.42.0/24" "fc00::/64" ];
@@ -24,6 +25,11 @@ in
       allowedIPsAsRoutes = mkOption {
         type = types.bool;
         default = true;
+      };
+
+      preferIPv4 = mkOption {
+        type = types.bool;
+        default = false;
       };
     };
   };
@@ -37,7 +43,9 @@ in
 
         peers = [
           {
-            endpoint = "[${data.hosts.wireguard.addr.pub.ipv6}]:23333";
+            endpoint = if cfg.preferIPv4
+              then "[${data.hosts.wireguard.addr.pub.ipv4}]:23333"
+              else "[${data.hosts.wireguard.addr.pub.ipv6}]:23333";
             persistentKeepalive = 25;
             inherit (cfg) allowedIPs;
             inherit (data.hosts.wireguard.wg) publicKey;
