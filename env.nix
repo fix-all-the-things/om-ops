39bad91b4156a2276d7025d089c1cc4ca08174b5
Vlastimil Holer <vlastimil.holer@gmail.com>
2021-07-13 21:39:17 +0200

    env: enable access to jm ls

--- env.nix
+++ env.nix
@@ -59,7 +59,7 @@ in
   };
 
   users.extraUsers.root.openssh.authorizedKeys.keys =
-    with import ./ssh-keys.nix; [ deploy ln mm srk ms vh ];
+    with import ./ssh-keys.nix; [ deploy ln mm srk ms vh jm ls ];
 
   system.stateVersion = lib.mkDefault "18.09";
 }
